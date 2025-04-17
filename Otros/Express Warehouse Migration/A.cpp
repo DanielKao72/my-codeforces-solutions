8c90f4d91d2d1219255a7a4d9e466dedad0954e8
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2025-04-17 15:15:25 -0600

    feat: :hammer: add solution for Express Warehouse Migration problem

---
+++ Otros/Express Warehouse Migration/A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define el "\n"
+#define ll long long
+#define yes cout << "YES" << el
+#define no cout << "NO" << el
+#define vll vector<ll>
+#define rep(i,a,b) for(ll i=a; i<b; i++)
+#define mll map<ll,ll>
+#define bob cout<<"Bob"<<el
+#define alice cout<<"Alice"<<el
+#define ALL(x) x.begin(), x.end() 
+
+void solve();
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll t; 
+    //cin>>t;
+    t = 1;
+    
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
+void solve(){
+    ll n, m; cin>>n>>m;
+    cout << (n + m - 1) / m << el;
+    return;
+}
