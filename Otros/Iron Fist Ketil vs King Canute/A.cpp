0bdd93900082ae069eafc69349fc8ea03b31b3b3
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2025-04-23 00:49:12 -0600

    feat: :hammer: add solution for Iron Fist Ketil vs King Canute problem

---
+++ Otros/Iron Fist Ketil vs King Canute/A.cpp
@@ -0,0 +1,53 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define el "\n"
+#define ll long long
+#define yes cout << "YES" << el
+#define no cout << "NO" << el
+#define vll vector<ll>
+#define rep(i,a,b) for(ll i=a; i<b; i++)
+#define mll map<ll,ll>
+#define bob cout<<"Bob"<<el
+#define alice cout<<"Alice"<<el
+
+void solve();
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    ll t; 
+    //cin>>t;
+    t = 1;
+    
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
+void solve(){
+    ll n, m, k; cin>>n>>m>>k;
+    ll cantidadMinimaKanil = k * m;
+
+    if(n >= cantidadMinimaKanil) cout<<"Iron fist Ketil"<<el;
+    else cout<<"King Canute"<<el;
+
+    return;
+} 
